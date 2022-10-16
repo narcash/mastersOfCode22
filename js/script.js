d492ee45e1d8725e80b55613916639954bbaf065
Nariman <mirmanov1@mail.ru>
2022-10-16 10:38:30 +0300

    feat: first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// let name = 'Nar';
+// console.log(`My name is ${name}`);
+// name = 'Nariman';
+// console.log(`My name is ${name}`);
+
+// const user = {
+//     name: 'Nar',
+//     gender: 'male',
+//     isMarried: false,
+//     age: 20
+// }
+// console.log(user);
+// user.name = 'Nariman'
+// console.log(user);
+
+// const colors = ['red', 'green', 'yellow']
+// console.log(colors);
+// colors.push('black')
+// console.log(colors);
+
---
+++ js/types.js
@@ -0,0 +1,77 @@
+// Primitive Data types
+
+// String
+// const someText = "Some Text";
+// console.log(typeof someText);
+
+// Number
+// const someNumber = 11;
+// console.log(typeof someNumber);
+
+// Boolean true or false
+// const someBoolean = true;
+// console.log(typeof someBoolean);
+
+// Null
+// const someNull = null;
+// console.log(typeof someNull);
+
+// Undefined
+// let someUndefined;
+// console.log(typeof someUndefined);
+
+// Symbol
+// const someSymbol = Symbol();
+// console.log(typeof someSymbol);
+
+// Reference data types
+
+// Array
+// const someArray = [1, 'hello', 2];
+// console.log(typeof someArray);
+
+// Object Literal
+// const someObjectLiteral = {
+//     firstProperty: 1,
+//     ssecondProperty: 'two',
+//     thirdProperty: true
+// };
+// console.log(typeof someObjectLiteral);
+
+// Function
+// const someFunction = new Function();
+// console.log(typeof someFunction);
+
+// Date
+// const someDate = new Date();
+// console.log(typeof someDate);
+
+// 14 Numbers Math
+
+const firstNumber = 50;
+const secondNumber = 7;
+let result;
+
+// Simple Math Operations
+result = firstNumber + secondNumber;
+result = firstNumber - secondNumber;
+result = firstNumber * secondNumber;
+result = firstNumber / secondNumber;
+result = firstNumber % secondNumber;
+
+// Math object
+result = Math.PI;
+result = Math.round(3.5);
+result = Math.ceil(2.3);
+result = Math.floor(2.9);
+result = Math.sqrt(9);
+result = Math.abs(-7);
+result = Math.pow(3, 4);
+result = Math.min(3, 4, 9, 2, 8, 82, -4);
+result = Math.max(3, 4, 9, 2, 8, 82, 900);
+result = Math.floor(Math.random() * 100);
+
+
+
+
+console.log(result);
